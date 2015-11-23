d0c09419745d7107caa36ffec7d2c1b53f51c50e
Vincent Wochnik <v.wochnik@gmail.com>
2015-11-23 19:30:53 +0100

    remove compass.rb file

--- lib/jekyll-compass.rb
+++ lib/jekyll-compass.rb
@@ -1,6 +1,27 @@
 
-# This is simply a router file. We don't do anything here except require the
-# *actual* file for our gem. This is the file that jekyll requires by default.
-# Ideally it shouldn't exist at all.
-# @see http://guides.rubygems.org/name-your-gem/
-require 'jekyll/compass'
+# This file serves as the main entry point for the plugin. We simply include
+# all the various parts of the plugin so that everything just works.
+
+# Jekyll is the static site builder that we are making a plugin for.
+#
+# @see http://jekyllrb.com/
+module Jekyll
+  # Sass is a set of extensions to CSS that allows for things like mixins
+  # and variables. Compass is a library of Sass functions and code for use
+  # with your own websites.
+  #
+  # @see http://sass-lang.com/
+  # @see http://compass-style.org/
+  module Compass
+  end
+end
+
+# Require dependencies
+require 'sass/plugin'
+require 'compass'
+require 'compass/commands'
+
+# Internal requires
+%w{compass_configuration compass_file generator compass_app_integration}.each do |f|
+  require "jekyll/compass/#{f}"
+end
